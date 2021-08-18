5d365e12874ce7d2dc5b3879142b9bd4b78a4d4f
Atryom <238136@mail.ru>
2021-08-18 14:47:51 +0300

    Add more comments

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -4,48 +4,53 @@ const shortid = require('shortid');
 const Link = require('../models/Link');
 const auth = require('../middleware/auth.middleware');
 const router = Router();
-
+// роут для пост запроса на генерацию ссылки
 router.post('/generate', auth, async (req, res) => {
   try {
-
+    // берём установленный урл
     const baseUrl = config.get('baseUrl');
+    // берём данные из формы
     const {from} = req.body;
-
+    // генерируем уникальный код для новой ссылки
     const code = shortid.generate();
-
+    // ищем в БД совпадения с введёной ссылкой
     const existing = await Link.findOne({ from });
-
+    // если такая ссылка уже существует - возвращаем её
     if (existing) {
       return res.json({ link: existing });
     }
-
+    // добавляем к установленному урл сгенерированный уникальный код
     const to = baseUrl + '/t/' + code;
-
+    // создаём объект ссылки с параметрами
     const link = new Link({
       code, to, from, owner: req.user.userId
     });
-
+    // сохраняем ссылку в БД
     await link.save();
-
+    // отдаём 201 с созданной ссылкой
     res.status(201).json(link);
 
   } catch (e) {
     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова'});
   }
 });
-
+// роут для получения списка всех ссылок
 router.get('/', auth,  async (req, res) => {
   try {
+    // ищем все ссылки, закреплённые за авторизованным пользователем
     const links = await Link.find({ owner: req.user.userId });
+    // отдаём список ссылок
     res.json(links);
   } catch (e) {
     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова'});
   }
 });
-
+// роут для запроса на ссылку с определенным айди
 router.get('/:id', auth, async (req, res) => {
   try {
+    // ищем в БД ссылку с таким айди
     const link = await Link.findById(req.params.id);
+    // отдаём найденную ссылку
     res.json(link);
   } catch (e) {
     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова'});
--- routes/redirect.routes.js
+++ routes/redirect.routes.js
@@ -2,17 +2,18 @@ const {Router} = require('express');
 const Link = require('../models/Link');
 const router = Router();
 
+// роут для редиректа по короткой ссылке
 router.get('/:code', async (req, res) => {
   try {
-
+    // ищем в БД ссылку с уникальным кодом
     const link = await Link.findOne({ code: req.params.code });
-
+    // если ссылка найдена, то увеличиваем количество переходов, сохраняем и перенаправляем на соответствующий адрес
     if (link) {
       link.clicks++;
       await link.save();
       return res.redirect(link.from);
     }
-
+    // если ссылки нет - отдаём 404
     res.status(404).json('Ссылка не найдена');
 
   } catch (e) {
